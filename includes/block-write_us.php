d645f6b4163a45892354a57e5e894ca938a41bea
IlyaKhazan <p9067700518@gmail.com>
2021-03-11 12:36:59 +0300

    Страница Стартапам

---
+++ includes/block-write_us.php
@@ -0,0 +1,15 @@
+<section class="write-us">
+    <div class="write-us__wrap">
+        <h3 class="title__third  write-us__title">Напишите нам</h3>
+        <form class="form" action="#" method="post">
+            <input type="text" name="name" placeholder="Как вас зовут?">
+            <input type="email" name="email" placeholder="Введите ваш e-mail">
+            <div class="confidentiality  write-us__checkbox">
+                <input type="checkbox" name="accept" id="write-us-accept" checked>
+                <label for="write-us-accept">Принимаю условия</label>
+                <a class="confidentiality__link" href="#">политики конфиденциальности</a>
+            </div>
+            <button type="submit" class="btn  btn--big  btn--accent  write-us__btn">Связаться со мной</button>
+        </form>
+    </div>
+</section>
--- template-startups.php
+++ template-startups.php
@@ -9,7 +9,132 @@ get_header();
 ?>
 
 <main>
-    
+    <section class="main-offer main-offer--startup">
+        <div class="container">
+            <h1 class="title title__strong">
+                <div class="title__bullet"><span class="title__colored">online</span> <span class="title__colored">инкубатор</span> </div>
+                <div>для <span class="title__colored">стартапов</span></div>
+            </h1>
+            <div class="main-offer__text-block">
+                <p>Вы ищете возможность создать свой стартап
+                    или найти новый проект для своей
+                    компании, тогда вы в нужном месте.
+                    Каждый день мы обучаем
+                    основателей, создаем
+                    проекты и </p>
+            </div>
+            <?php get_template_part('/includes/block', 'sign_up'); ?>
+        </div>
+    </section>
+    <section class="first-step">
+        <div class="container">
+            <div class="first-step__inner-wrap">
+                <h2 class="title__secondary"><span class="title__colored">Сделай</span> первый шаг</h2>
+                <p class="first-step__text-block">
+                    Пройди наш акселератор* со своей идеей,
+                    убедись, что она жизнеспособна, получи
+                    первые продажи и мы проинвестируем в
+                    создание твоего MVP**.
+                </p>
+                <a class="btn btn--small" href="#">Подать заявку</a>
+                <div class="first-step__legend">
+                    <p>
+                        *Подходит для любого
+                        бизнеса, включая
+                        традиционный.</p>
+
+                    <p>
+                        **Минимально жизнеспособный
+                        продукт — продукт, обладающий
+                        минимальными, но достаточными
+                        для удовлетворения первых
+                        потребителей функциями.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="team">
+        <div class="container">
+
+            <h2 class="title__secondary">Команда для твоего проекта</h2>
+            <p class="team__text-block">После прохождения акселератора
+                вы продолжите заниматься развитием проекта
+                и с командой параллельно создавать MVP.
+            </p>
+
+            <div class="team__visuals">
+                <div class="team__visual-item team__visual-item--designer"></div>
+
+                <div class="team__visual-item team__visual-item--pm"></div>
+
+                <div class="team__visual-item team__visual-item--developer"></div>
+
+
+            </div>
+        </div>
+    </section>
+    <section class="prospects">
+        <div class="container">
+            <h2 class="title__secondary">Ваши перспективы после инкубатора</h2>
+            <ul class="prospects__list">
+                <li class="prospects__item"><span>Самостоятельное развитие</span> -
+                    при наличии продукта
+                    и с подтвержденным спросом
+                    вы сможете начать самостоятельно
+                    зарабатывать на своей идее.</li>
+                <li class="prospects__item"><span>Корпоративный&nbsp;акселератор</span> -
+                    множество компаний ищут
+                    проекты на стадии первых
+                    продаж - это ваш шанс
+                    масштабировать свой проект.</li>
+                <li class="prospects__item"><span>Привлечение инвестиций</span> -
+                    при наличии MVP, команды
+                    и первых продаж у вас
+                    больше шансов привлечь
+                    инвестора в ваш проект.</li>
+            </ul>
+            <a class="btn btn--green btn--big" href="#">Перейти в акселератор</a>
+        </div>
+    </section>
+    <section class="ideas">
+        <div class="container">
+            <div class="ideas__inner-wrap">
+                <h2 class="title__secondary">Ищем ваши идеи</h2>
+                <p class="ideas__text-block">Проекты из разных сфер объединяет очень
+                    многое: стратегия развития, целевая аудитория,
+                    техника продаж и работа с конкурентами. Мы
+                    созданы, чтобы помочь вам разобраться в этом.
+                </p>
+                <div class="ideas__sign-up"><?php get_template_part('/includes/block', 'sign_up'); ?></div>
+            </div>
+        </div>
+    </section>
+    <!--<section class="projects">
+       Наши проекты
+    </section>-->
+    <section class="feedback">
+        <div class="container">
+            <div class="feedback__grid">
+                <div class="feedback__inner-wrap">
+                    <h2 class="title__secondary">Ищете проект для пилота или&nbsp;инвестиций</h2>
+                    <p class="feedback__text-block">Мы проводим скоринг новаторов и доводим
+                        проект до первых продаж, чтобы вы были
+                        уверены в своих инвестициях, а также
+                        помогаем с интеграцией проектов в бизнес.
+                    </p>
+                </div>
+                <div class="feedback__form-wrap">
+                    <div class="feedback__write-us">
+                        <?php get_template_part('/includes/block', 'write_us'); ?></div>
+                    <div class="feedback__get-news">
+                        <?php get_template_part('/includes/block', 'getnews'); ?>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
 </main>
 
-<?php get_footer();?>
+<?php get_footer(); ?>
