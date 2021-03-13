0fee15d080788b9419892fd77bfb61a42329f422
IlyaKhazan <p9067700518@gmail.com>
2021-03-13 10:37:53 +0300

    Стартапам

--- includes/block-write_us.php
+++ includes/block-write_us.php
@@ -4,10 +4,10 @@
         <form class="form" action="#" method="post">
             <input type="text" name="name" placeholder="Как вас зовут?">
             <input type="email" name="email" placeholder="Введите ваш e-mail">
-            <div class="confidentiality  write-us__checkbox">
+            <div class="confidentiality">
                 <input type="checkbox" name="accept" id="write-us-accept" checked>
-                <label for="write-us-accept">Принимаю условия</label>
-                <a class="confidentiality__link" href="#">политики конфиденциальности</a>
+                <label for="write-us-accept">Принимаю условия
+                    <a class="confidentiality__link" href="#">политики конфиденциальности</a></label>
             </div>
             <button type="submit" class="btn  btn--big  btn--accent  write-us__btn">Связаться со мной</button>
         </form>
