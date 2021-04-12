124c2ba816d32eca96d2bf50fb2ef42f04d60fd6
Elena Christensen <olenachristensen777@gmail.com>
2021-04-12 08:07:20 +0200

    clouds fixing

--- page-socrat-cup.php
+++ page-socrat-cup.php
@@ -18,24 +18,28 @@ get_header('socrat'); ?>
         </div>
     </section>
     <section class="cup-start">
-        <div class="container">
-            <h2 class="title__secondary">
-                Старт кубка "Сократа" <br>
-                <span class="title__colored">5 апреля - 5 мая</span>
-            </h2>
-            <p class="cup-start__text-block">
-                Мы стремимся поддерживать
-                талантливых людей и специально
-                для вас мы организовали
-                региональный конкурс для
-                школьников "Кубок Сократа".
-                Это уникальная возможность
-                с помощью знаний получить
-                гранд на обучение одной из
-                современных профессий.
-            </p>
-            <button class="btn-socrat">Принять участие</button>
+        <img class="clouds" src="/wp-content/themes/js-corp/images/svg/clouds.svg">
+        <div class="cup-start__background-wrap">
+            <div class="container">
+                <h2 class="title__secondary">
+                    Старт кубка "Сократа" <br>
+                    <span class="title__colored">5 апреля - 5 мая</span>
+                </h2>
+                <p class="cup-start__text-block">
+                    Мы стремимся поддерживать
+                    талантливых людей и специально
+                    для вас мы организовали
+                    региональный конкурс для
+                    школьников "Кубок Сократа".
+                    Это уникальная возможность
+                    с помощью знаний получить
+                    гранд на обучение одной из
+                    современных профессий.
+                </p>
+                <button class="btn-socrat">Принять участие</button>
+            </div>
         </div>
+        <img class="clouds  clouds--bottom" src="/wp-content/themes/js-corp/images/svg/clouds.svg">
     </section>
     <section class="cup-partners">
         <div class="container">
@@ -74,54 +78,58 @@ get_header('socrat'); ?>
     </section>
 
     <section class="cup-award">
-        <div class="container">
-            <h2 class="title__secondary">
-                Приз победителю
-                <br>
-                <span class="title__colored">гранд на обучение!</span>
-            </h2>
-            <ul class="cup-award__list">
-                <li class="cup-award__item">
-                    <h2 class="title__secondary title__colored">1 место</h2>
-                    <h3 class="cup-award__item-title">Гранд <br> на обучение</h3>
-                    <p class="cup-award__text-block">
-                        Победитель, занявший первое место,
-                        получит гранд на полное обучение
-                        одной из профессий*
-                    </p>
+        <img class="clouds" src="/wp-content/themes/js-corp/images/svg/clouds.svg">
+        <div class="cup-award__background-wrap">
+            <div class="container">
+                <h2 class="title__secondary">
+                    Приз победителю
+                    <br>
+                    <span class="title__colored">гранд на обучение!</span>
+                </h2>
+                <ul class="cup-award__list">
+                    <li class="cup-award__item">
+                        <h2 class="title__secondary title__colored">1 место</h2>
+                        <h3 class="cup-award__item-title">Гранд <br> на обучение</h3>
+                        <p class="cup-award__text-block">
+                            Победитель, занявший первое место,
+                            получит гранд на полное обучение
+                            одной из профессий*
+                        </p>
 
-                    <p class="cup-award__legend">
-                        * Курс обучения можно выбрать в мобильном
-                        приложении Sokrat.</p>
-                </li>
-                <li class="cup-award__item">
-                    <h2 class="title__secondary title__colored">2 место</h2>
-                    <h3 class="cup-award__item-title">Гранд <br> на обучение</h3>
-                    <p class="cup-award__text-block">
-                        Победитель, занявший второе место,
-                        получит гранд в размере <span>10.000&nbsp;руб.</span>
-                        и сможет потратить его на один или
-                        несколько курсов*
-                    </p>
-                    <p class="cup-award__legend">
-                        * Курс обучения можно выбрать в мобильном
-                        приложении Sokrat.</p>
-                </li>
-                <li class="cup-award__item">
-                    <h2 class="title__secondary title__colored">3 место</h2>
-                    <h3 class="cup-award__item-title">Гранд <br> на обучение</h3>
-                    <p class="cup-award__text-block">
-                        Победитель, занявший второе место,
-                        получит гранд в размере <span>5.000&nbsp;руб.</span>
-                        и сможет потратить его на один или
-                        несколько курсов*
-                    </p>
-                    <p class="cup-award__legend">
-                        * Курс обучения можно выбрать в мобильном
-                        приложении Sokrat.</p>
-                </li>
-            </ul>
+                        <p class="cup-award__legend">
+                            * Курс обучения можно выбрать в мобильном
+                            приложении Sokrat.</p>
+                    </li>
+                    <li class="cup-award__item">
+                        <h2 class="title__secondary title__colored">2 место</h2>
+                        <h3 class="cup-award__item-title">Гранд <br> на обучение</h3>
+                        <p class="cup-award__text-block">
+                            Победитель, занявший второе место,
+                            получит гранд в размере <span>10.000&nbsp;руб.</span>
+                            и сможет потратить его на один или
+                            несколько курсов*
+                        </p>
+                        <p class="cup-award__legend">
+                            * Курс обучения можно выбрать в мобильном
+                            приложении Sokrat.</p>
+                    </li>
+                    <li class="cup-award__item">
+                        <h2 class="title__secondary title__colored">3 место</h2>
+                        <h3 class="cup-award__item-title">Гранд <br> на обучение</h3>
+                        <p class="cup-award__text-block">
+                            Победитель, занявший второе место,
+                            получит гранд в размере <span>5.000&nbsp;руб.</span>
+                            и сможет потратить его на один или
+                            несколько курсов*
+                        </p>
+                        <p class="cup-award__legend">
+                            * Курс обучения можно выбрать в мобильном
+                            приложении Sokrat.</p>
+                    </li>
+                </ul>
+            </div>
         </div>
+        <img class="clouds  clouds--bottom" src="/wp-content/themes/js-corp/images/svg/clouds.svg">
     </section>
     <section class="cup-guarantee">
         <div class="container">
